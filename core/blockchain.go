93450726c5570cd33bc59a13ce64a19eabd3cac8
adair <adairchan.dream@gmail.com>
2024-10-21 10:53:42 +0700

    feat(persistence-and-cli): persist data with boltDb
    use boltDb to store boNewBlockChain & AddBlock
    
    Closes #6

--- core/blockchain.go
+++ core/blockchain.go
@@ -1,17 +1,66 @@
 package core
 
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+const dbFile = "blockchain_%s.db"
+const blocksBucket = "blocks"
+
 type Blockchain struct {
-	blocks []*Block
+	//blocks []*Block
+	tip []byte
+	// TODO to support more db
+	db *bolt.DB
 }
 
 func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
+	// get last hash from db
+	var lastHash []byte
+	err := chain.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// create new block and store
+	newBlock := NewBlock(data, lastHash)
+	err = chain.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err = b.Put(newBlock.Hash, newBlock.Serialize())
+		err = b.Put([]byte("l"), newBlock.Hash)
+		chain.tip = newBlock.Hash
+		return nil
+	})
 }
 
 func NewBlockChain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	// Open a DB file.
+	db, err := bolt.Open(dbFile, 0600, nil)
+	err = db.Update(func(tx *bolt.Tx) error {
+		// Check if there’s a blockchain stored in it.
+		b := tx.Bucket([]byte(blocksBucket))
+		// if no, Create a new Blockchain instance with its tip pointing at the genesis block
+		if b == nil {
+			genesisBlock := NewGenesisBlock()
+			b, err = tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+			err = b.Put([]byte("l"), genesisBlock.Hash)
+			tip = genesisBlock.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	return &Blockchain{tip, db}
 }
 
 // NewGenesisBlock when the chain created, init GenesisBlock
